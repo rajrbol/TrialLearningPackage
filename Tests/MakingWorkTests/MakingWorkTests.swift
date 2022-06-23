560db294a5a0172ca8cf9015d7fbfeaa43cbc63b
rajrbol <raj.raval@lji.io>
2022-06-23 17:39:38 +0530

    Added SPM Support

---
+++ Tests/MakingWorkTests/MakingWorkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MakingWork
+
+final class MakingWorkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MakingWork().text, "Hello, World!")
+    }
+}
