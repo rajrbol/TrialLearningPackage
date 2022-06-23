083815c0cd0b1284cfe83d1b5b3e65e8e08eb378
rajrbol <raj.raval@lji.io>
2022-06-23 16:38:04 +0530

    Added Example

--- Example/MakingWork/ViewController.swift
+++ Example/MakingWork/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import MakingWork
 
 class ViewController: UIViewController {
+    
+    private lazy var lazyView: UIView = {
+        let newView = UIView()
+        newView.translatesAutoresizingMaskIntoConstraints = false
+        return newView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(lazyView)
+        NSLayoutConstraint.activate([
+            lazyView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            lazyView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            lazyView.heightAnchor.constraint(equalToConstant: 100),
+            lazyView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2)
+        ])
+        lazyView.backgroundColor = .orange
+        lazyView.addShadow(cornerRadius: 13, shadowColor: .blue, shadowOffset: CGSize(width: 1, height: 1), shadowOpacity: 0.3, shadowRadius: 6)
     }
 
     override func didReceiveMemoryWarning() {
