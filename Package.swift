8c69f4ae1c23e761d54ee0a2e9591e3114b8397b
rajrbol <raj.raval@lji.io>
2022-06-24 12:49:20 +0530

    Added package path

--- Package.swift
+++ Package.swift
@@ -20,9 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MakingWork",
-            dependencies: []),
+            dependencies: [],
+            path: "MakingWork/Sources/MakingWork"),
         .testTarget(
             name: "MakingWorkTests",
-            dependencies: ["MakingWork"]),
+            dependencies: ["MakingWork/Sources/MakingWorkTests"]),
     ]
 )
