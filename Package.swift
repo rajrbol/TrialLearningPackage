bc4ad481eefc6f5fb9c25fd5cd93245741d63ee0
rajrbol <raj.raval@lji.io>
2022-06-24 13:16:55 +0530

    Remove path in Package.swift

--- Package.swift
+++ Package.swift
@@ -20,11 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MakingWork",
-            dependencies: [],
-            path: "Sources/"),
+            dependencies: []),
         .testTarget(
             name: "MakingWorkTests",
-            dependencies: [],
-            path: "Tests/"),
+            dependencies: []),
     ]
 )
